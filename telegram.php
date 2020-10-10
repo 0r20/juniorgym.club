3afd65f91ad6c8a373cb543cae68133dc2b89423
unknown <0r20@mail.ru>
2020-10-10 21:26:52 +0300

    Залив всего сайта на гитхаб

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "1110590194:AAFw1q-nycHTdHfnMy8-g9-Hd_LUh5HvP3g";
+$chat_id = "-399318099";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email:' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
+?>
